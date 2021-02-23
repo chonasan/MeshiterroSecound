fbd892c57b93eb625cb6aeb3dbe91ecb32916ac6
chonasan <tk24120824@gmail.com>
2021-02-23 15:23:15 +0000

    コメント

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4c72f27efa1746476e42a63e665b318c071dc5a721ac27caf2b307d16b53ca47b41c921dc89319c29a7ea7381a061468430ec8d79d4268236abe3fbac42196f2'
